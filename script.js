309ee2578dc149d7f23d87d44fb0ad47661b9225
EBRIMA SONKO <82295266+ibra188@users.noreply.github.com>
2022-01-12 18:09:33 +0100

    added new files style the form in contact page

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const navLink = document.querySelector('.nav__link');
 hamburger.addEventListener('click', () => {
     navLink.classList.toggle('hide');
 });
+
+const nameInput = document.querySelector('[name="name"]');
+nameInput.addEventListener('invalid', () => {
+   nameInput.setCustomValidity('Please enter a valid name.');
+});
